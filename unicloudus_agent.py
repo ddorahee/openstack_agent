3537c3713a7a116d01e47d24b23d3e785346abe6
ddorahee <jomin0613@gmail.com>
2020-02-15 03:13:23 -0500

    asd

--- unicloudus_agent.py
+++ unicloudus_agent.py
@@ -2,6 +2,9 @@ import http.server
 import socketserver
 import re
 import ast
+import os
+import time
+from datetime import datetime
 from checkfile.openstack_checkfile_manage import openstack_check_all_file
 from data.openstack_data_manage import openstack_data_all
 
@@ -9,6 +12,10 @@ from data.openstack_data_manage import openstack_data_all
 PORT = 35358
 
 class CustomHandler(http.server.SimpleHTTPRequestHandler):
+
+    def get_now_time_string(self):
+        date_time = datetime.fromtimestamp(time.time())
+        return date_time.strftime("%Y-%m-%d_%H:%M:%S")
 
     def do_POST(self):
         if None != re.search('/openstack/*', self.path):
@@ -27,7 +34,19 @@ class CustomHandler(http.server.SimpleHTTPRequestHandler):
                 self.wfile.write(str(openstack_data).encode())
 
             elif resource == "checklist" :
+                path = os.path.dirname(os.path.abspath(__file__)) + "/log"
                 openstack_checklist = openstack_check_all_file()
+
+                checklist = {
+                   'cloud_name': body['cloud_name'],
+                   'vendor': body['vendor'],
+                   'time': self.get_now_time_string(),
+                   'checklist': openstack_checklist
+                }
+
+                checklist_file = open(path + '/' + self.get_now_time_string() + '.chk', 'w')
+                checklist_file.write(str(checklist))
+                checklist_file.close()
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
